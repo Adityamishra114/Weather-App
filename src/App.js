a66f5099796e2c811260dc7860ef74ce346ab505
Adityamishra114 <amishradbg1997@gmail.com>
2022-10-14 23:02:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import ShowTemp from "./components/ShowTemp";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState({
+    description: "",
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0,
+    humidity: 0,
+    sunrise: 0,
+    sunset: 0,
+    country: "",
+  });
+
+  const handleClick = () => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fa7f2c19748cf687299cd77b6f79bfca`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setData({
+          description: response.data.weather[0].description,
+          temp: response.data.main.temp,
+          temp_max: response.data.main.temp_max,
+          temp_min: response.data.main.temp_min,
+          humidity: response.data.main.humidity,
+          sunrise: response.data.sys.sunrise,
+          sunset: response.data.sys.sunset,
+          country: response.data.sys.country,
+        });
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container text-center my-2">
+        <h1>Weather App</h1>
+        <input
+          type="text"
+          className="form-control border"
+          value={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <button
+          className="btn btn-primary my-2 border"
+          type="submit"
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          get temp
+        </button>
+      </div>
+
+      <ShowTemp text={data}></ShowTemp>
+    </>
   );
 }
 
